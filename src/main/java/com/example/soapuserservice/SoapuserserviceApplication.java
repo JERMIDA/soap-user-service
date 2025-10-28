ea48b7a7f43a918ec93c8297e2d4270e0603cbf1
RushdiNura <rushdinura144@gmail.com>
2025-10-28 22:00:35 +0300

    Update SoapuserserviceApplication.java
    Edited SoapuserserviceApplication

--- src/main/java/com/example/soapuserservice/SoapuserserviceApplication.java
+++ src/main/java/com/example/soapuserservice/SoapuserserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SoapuserserviceApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SoapuserserviceApplication.class, args);
 	}
 
